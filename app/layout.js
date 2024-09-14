96c1c65ad5c4c20d4e59cbfc2828cc7923e71385
Abed Alawieh <aalawieh@borninteractive.com>
2024-09-14 10:14:06 +0300

    Added the script

--- app/layout.js
+++ app/layout.js
@@ -1,23 +1,22 @@
-import { GoogleTagManager } from '@next/third-parties/google';
-import { Inter } from 'next/font/google';
-import { ToastContainer } from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css';
-import Footer from './components/footer';
-import Navbar from './components/navbar';
-import './css/card.scss';
-import './css/globals.scss';
-const inter = Inter({ subsets: ['latin'] });
+import { GoogleTagManager } from "@next/third-parties/google";
+import { Inter } from "next/font/google";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import Footer from "./components/footer";
+import Navbar from "./components/navbar";
+import "./css/card.scss";
+import "./css/globals.scss";
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
-  title: 'Portfolio of Abed Alawieh -  Full Stack Software Developer',
-  description: 'This is the portfolio of Abed Alawieh. A Full Stack Software Developer.',
-  
+  title: "Portfolio of Abed Alawieh -  Full Stack Software Developer",
+  description:
+    "This is the portfolio of Abed Alawieh. A Full Stack Software Developer.",
 };
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-   
       <body className={inter.className}>
         <ToastContainer />
         <main className="min-h-screen relative mx-auto px-6 sm:px-12 lg:max-w-[70rem] xl:max-w-[76rem] 2xl:max-w-[92rem] text-white">
@@ -27,6 +26,11 @@ export default function RootLayout({ children }) {
         <Footer />
       </body>
       <GoogleTagManager gtmId={process.env.NEXT_PUBLIC_GTM} />
+      <script
+        async
+        src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-5453450507417118"
+        crossorigin="anonymous"
+      ></script>
     </html>
-  )
-};
+  );
+}
